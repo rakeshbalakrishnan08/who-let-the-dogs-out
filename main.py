61779bf359ccf8b36f5a6e53c3e6e5f8a7dc2814
Balakrishnan <Rakesh.Balakrishnan@McKesson.com>
2019-02-13 08:15:45 -0800

    our first dog class program

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 from dog import Dog
-from statistics import standard_deviation
 
-dogs = []
+d1 = Dog('fido', 3, 'male')
+d2 = Dog('sally', 5, 'female')
+
+#dogs = [d1, d2]
+#dogdict = {d1.name: d1, d2.name: d2}
+
+print('Before - ', d1.name, d1.weight)
+d1.eat()
+print('After eating -',  d1.name, d1.weight)
+d1.play()
+print('After playing -',  d1.name, d1.weight)
+
+darray = []
+avgage = 0
+
 with open('dogs.txt') as f:
     for line in f:
-        name, age, sex = line.strip().split(',')
-        age = float(age)
-        dog = Dog(name, age, sex)
-        dogs.append(dog)
+        dogs = line.strip().split(',')
+        print(dogs)
+        d1 = Dog(dogs[0],int(dogs[1]),dogs[2])
+        darray.append(d1)
+        print(len(darray))
+        avgage += d1.age
 
-ages = [dog.age for dog in dogs]
-avgage = sum(ages) / len(ages)
-stdage = standard_deviation(ages)
-print(dogs, avgage, stdage)
+avgage = avgage/len(darray)
+result = 0
+for d in darray:
+    result += (avgage - d.age)**2
+
+print(result)
+variance = result**0.5
+stddev = variance**0.5
+print('avg age - ', avgage)
+print('std dev - ', stddev)
+
